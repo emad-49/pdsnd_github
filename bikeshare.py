a6fdb69469601ca9ed85f195b556445cf3b27a40
emad-49 <emadalharbi49@gmail.com>
2022-12-25 21:52:59 +0300

    formate the code : add space

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,9 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-CITY_NAME =  ['chicago','new york city','washington']
+CITY_NAME = ['chicago','new york city','washington']
+
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
